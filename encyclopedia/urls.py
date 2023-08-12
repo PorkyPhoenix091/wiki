2a168e4a5095e930062909cf87d9f88bdabaf5a8
Anthony_ <anthonygiuliano59@gmail.com>
2023-08-12 19:55:12 +0200

    added urls views

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:entry>", views.entry,name="entry"),
+    path("random", views.entry, name="random"),
+    path("newpage", views.entry, name="newpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from markdown2 import Markdown
+markdowner = Markdown()
 
 from . import util
 
@@ -7,4 +9,10 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def entry(request, entry):
+    return render(request,"encyclopedia/entry.html",{
+        "code": markdowner.convert(util.get_entry(entry)),
+        "entry": entry
+    })
+def newpage(request):
+    return render(request,"encyclopedia/newpage.html",{})
